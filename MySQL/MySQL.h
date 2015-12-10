dd6853db9948af8f9cdfa8a42326d4beb9ef0c40
David Ask <david@formbound.com>
2015-12-10 14:15:40 +0100

    Initial Commit

---
+++ MySQL/MySQL.h
@@ -0,0 +1,19 @@
+//
+//  MySQL.h
+//  MySQL
+//
+//  Created by David Ask on 10/12/15.
+//  Copyright © 2015 Formbound. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MySQL.
+FOUNDATION_EXPORT double MySQLVersionNumber;
+
+//! Project version string for MySQL.
+FOUNDATION_EXPORT const unsigned char MySQLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySQL/PublicHeader.h>
+
+
