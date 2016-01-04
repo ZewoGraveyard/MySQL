50a28fd975ab2b4d87b57a8c6f4577fe38a3bfc5
David Ask <david@formbound.com>
2016-01-04 11:11:47 +0100

    Updated package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "MySQL",
     dependencies: [
-        .Package(url: "https://github.com/formbound/SQL.git", majorVersion: 0),
-        .Package(url: "https://github.com/formbound/CMySQL.git", majorVersion: 1)
+        .Package(url: "https://github.com/Zewo/SQL.git", majorVersion: 0),
+        .Package(url: "https://github.com/Zewo/CMySQL.git", majorVersion: 1)
     ]
 )
